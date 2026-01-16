e16a9b16dc5bfd72df6e5209b7dc4cf0de12f976
samuelchika <samuelemyrs@gmail.com>
2026-01-16 22:31:39 +0000

    Added netlify library to package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-  },
-  output: 'server',
-  integrations: [ 
-      icon()
-  ],
-  adapter: node({
-    mode: 'standalone',
-  }),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	output: 'server',
+	integrations: [icon()],
+	// adapter: node({
+	//   mode: 'standalone',
+	// }),
+	adapter: netlify(),
 });
